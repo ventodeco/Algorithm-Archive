18856ce46c48853b7793a36e60b70094d3e4a455
Yosep Novento Nugroho <42646158+ventodeco@users.noreply.github.com>
2024-06-20 22:58:51 +0700

    Time: 2 ms (88.04%), Space: 56.1 MB (19.89%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,45 +1,39 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        // [1, 2, 3, 4, 5]
-        // leftMap  = 1 -> n-1
-        // rightMap = 2 -> n
-        
-        Map<Integer, Integer> leftMap = new HashMap<>();
-        Map<Integer, Integer> rightMap = new HashMap<>();
-        
-        // rightMap
-        int rightIndex = nums.length - 1;
-        int rightResult = 1;
-        while (rightIndex > 0) {
-            rightResult *= nums[rightIndex];
-            rightMap.put(rightIndex, rightResult);
-            rightIndex--;
-        }
-        
-        // leftMap
-        int leftIndex = 0;
-        int leftResult = 1;
-        while(leftIndex < nums.length - 1) {
-            leftResult *= nums[leftIndex];
-            leftMap.put(leftIndex, leftResult);
-            leftIndex++;
-        }
-        
-        int[] resultList = new int[nums.length];
-        Integer multiplyResult = 1;
+        int[] productFromLeft = new int[nums.length];
+        int fromLeftTotal = 1;
         for (int i = 0; i < nums.length; i++) {
-            if (leftMap.get(i-1) != null) {
-                multiplyResult *= leftMap.get(i-1);
+            fromLeftTotal *= nums[i];
+            productFromLeft[i] = fromLeftTotal;
+        }
+
+        int[] productFromRight = new int[nums.length];
+        int fromRightTotal = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            fromRightTotal *= nums[i];
+            productFromRight[i] = fromRightTotal;
+        }
+
+        int[] result = new int[nums.length];
+        int leftResult;
+        int rightResult;
+        for (int i = 0; i < nums.length; i++) {
+            
+            if (i > 0) {
+                leftResult = productFromLeft[i - 1];
+            } else {
+                leftResult = 1;
             }
             
-            if (rightMap.get(i+1) != null) {
-                multiplyResult *= rightMap.get(i+1);
+            if (i < nums.length - 1) {
+                rightResult = productFromRight[i + 1];
+            } else {
+                rightResult = 1;
             }
-
-            resultList[i] = multiplyResult;
-            multiplyResult = 1;
+            
+            result[i] = leftResult * rightResult;
         }
-        
-        return resultList;
+
+        return result;
     }
 }
