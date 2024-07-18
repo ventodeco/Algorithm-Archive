d981bfca00200fbfc7d54e2632d7467ff85884dd
Yosep Novento Nugroho <42646158+ventodeco@users.noreply.github.com>
2024-07-18 09:43:05 +0700

    Time: 20 ms (6.38%), Space: 45.4 MB (8.22%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String reverseWords(String s) {
+        List<String> strList = new ArrayList<>();
+        
+        String str = "";
+        Boolean strStart = true;
+        Boolean strEnd = false;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ') {
+                str += s.charAt(i);
+            }
+
+            if (s.charAt(i) == ' ' && !str.isEmpty()) {
+                strList.add(str);
+                str = "";
+            }
+        }
+        
+        if (!str.isEmpty()) {
+            strList.add(str);
+        }
+        
+        String result = "";
+        for (int i = strList.size() - 1; i >= 0; i--) {
+            if (i == strList.size() - 1) {
+                result += strList.get(i);
+            } else {
+                result += " " + strList.get(i);
+            }
+        }
+        
+        return result;
+    }
+}
