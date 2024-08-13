0d8dc2eabbe737d5fcade1a5ce84f15fce776dc8
Yosep Novento Nugroho <42646158+ventodeco@users.noreply.github.com>
2024-08-13 23:57:50 +0700

    Time: 5 ms (53.88%), Space: 43.8 MB (29.50%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String simplifyPath(String path) {
+
+        String[] arr = path.split("/");
+        Stack<String> stack = new Stack();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].trim() != "") {
+                stack.push(arr[i]);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        while (!stack.isEmpty()) {
+            String s = stack.pop();
+
+            if (s.equals("..")) {
+                count++;
+            } else if (s.equals(".")) {
+                continue;
+            } else {
+                if (count == 0) {
+                    sb.insert(0, "/" + s);
+                } else {
+                    count--;
+                }
+            }
+        }
+        return sb.toString().length() == 0 ? "/" : sb.toString();
+    }
+}
