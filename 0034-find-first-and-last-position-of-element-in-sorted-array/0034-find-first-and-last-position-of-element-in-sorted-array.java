22cc5982577a407134ef83cf29779d67e17d3baf
Yosep Novento Nugroho <42646158+ventodeco@users.noreply.github.com>
2024-09-11 21:49:53 +0700

    Time: 0 ms (100.00%), Space: 45.9 MB (53.46%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,23 +1,47 @@
 class Solution {
+
     public int[] searchRange(int[] nums, int target) {
         
-        int low = -1;
-        int high = -1;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                if (low == -1) {
-                    low = i;
-                    high = i;
-                } else {
-                    high = i;
-                }
-            }
-            
-            if (target < nums[i]) {
-                break;
-            }
+        if (nums.length == 0) {
+            return new int[]{-1, -1};
         }
         
-        return new int[]{low, high};
+        int pivot = nums.length/2;
+        
+        int low = 0;
+        int high = nums.length - 1;
+        int mid = high/2;
+
+        while (low <= high) {
+
+            if (nums[mid] == target) {
+                break;
+            }
+
+            if (nums[mid] >= target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+
+            mid = (high+low)/2;
+        }
+
+        int left = mid;
+        int right = mid;
+        int leftResult = -1;
+        int rightResult = -1;
+
+        while (left >= 0 && target == nums[left]) {
+            leftResult = left;
+            left--;
+        }
+        
+        while (right < nums.length && target == nums[right]) {
+            rightResult = right;
+            right++;
+        }
+
+        return new int[]{leftResult, rightResult};
     }
 }
