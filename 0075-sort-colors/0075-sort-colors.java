97521d507c1fba86430d89efd4513525cf5c3f2d
Yosep Novento Nugroho <42646158+ventodeco@users.noreply.github.com>
2024-10-08 22:57:26 +0700

    Time: 0 ms (100.00%), Space: 42 MB (32.36%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,15 +1,27 @@
 class Solution {
     public void sortColors(int[] nums) {
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        int left = 0, current = 0, right = nums.length - 1, tempValue;
         
-        for (int i = 0; i < nums.length; i++) {
-            minHeap.add(nums[i]);
-        }
-        
-        int iterator = 0;
-        while (! minHeap.isEmpty()) {
-            nums[iterator] = minHeap.poll();
-            iterator++;
+        while (current <= right && current < nums.length) {
+
+            if (nums[current] == 0) {
+                tempValue = nums[current];
+                nums[current] = nums[left];
+                nums[left] = tempValue;
+                left++;
+                current++;
+                continue;
+            }
+            
+            if (nums[current] == 2) {
+                tempValue = nums[current];
+                nums[current] = nums[right];
+                nums[right] = tempValue;
+                right--;
+                continue;
+            }
+
+            current++;
         }
     }
 }
