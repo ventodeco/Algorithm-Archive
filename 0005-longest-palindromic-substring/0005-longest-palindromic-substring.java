72d091fe70f1a379047fb659c3ffda6cabd16dac
Yosep Novento Nugroho <42646158+ventodeco@users.noreply.github.com>
2024-10-09 21:25:31 +0700

    Time: 144 ms (35.61%), Space: 44.9 MB (58.00%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -3,7 +3,6 @@ class Solution {
 
         String result = "";
         Deque<Character> deque;
-        StringBuilder stringBuilder;
         Integer left, right;
 
         for (int i = 0; i < s.length(); i++) {
@@ -13,49 +12,38 @@ class Solution {
             deque.addFirst(s.charAt(i));
             left = i - 1;
             right = i + 1;
-            while (left >= 0 && right < s.length()) {
-                if (s.charAt(left) != s.charAt(right)) {
-                    break;
-                }
-
-                deque.addFirst(s.charAt(left));
-                deque.addLast(s.charAt(right));
-                left--;
-                right++;
-            }
-
-            if (deque.size() > result.length()) {
-                stringBuilder = new StringBuilder();
-                for (Character ch : deque) {
-                    stringBuilder.append(ch);
-                }
-                result = stringBuilder.toString();
-            }
+            result = checkPalindrome(left, right, s, deque, result);
 
             // even
             left = i;
             right = i + 1;
             deque = new ArrayDeque();
-            while (left >= 0 && right < s.length()) {
-                if (s.charAt(left) != s.charAt(right)) {
-                    break;
-                }
-
-                deque.addFirst(s.charAt(left));
-                deque.addLast(s.charAt(right));
-                left--;
-                right++;
-            }
-
-            if (deque.size() > result.length()) {
-                stringBuilder = new StringBuilder();
-                for (Character ch : deque) {
-                    stringBuilder.append(ch);
-                }
-                result = stringBuilder.toString();
-            }
+            result = checkPalindrome(left, right, s, deque, result);
         }
 
+        return result;
+    }
+    
+    private String checkPalindrome(int left, int right, String s, Deque<Character> deque, String result) {
+        while (left >= 0 && right < s.length()) {
+            if (s.charAt(left) != s.charAt(right)) {
+                break;
+            }
+
+            deque.addFirst(s.charAt(left));
+            deque.addLast(s.charAt(right));
+            left--;
+            right++;
+        }
+        
+        if (deque.size() > result.length()) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (Character ch : deque) {
+                stringBuilder.append(ch);
+            }
+            result = stringBuilder.toString();
+        }
+        
         return result;
     }
 }
