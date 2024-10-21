cff7999af1f4661e19ffb4ab7db904e10985467d
Yosep Novento Nugroho <42646158+ventodeco@users.noreply.github.com>
2024-10-22 00:26:37 +0700

    Time: 5 ms (32.80%), Space: 48.8 MB (93.29%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,36 +1,65 @@
 class Solution {
-    private int row;
-    private int column;
+    
+    private int[][] visited;
+    private char[][] grid;
+    private static int VISITED = 1;
+    private static int NOT_VISITED = 0;
+    private static Character ISLAND = '1';
+    private int counter;
+    
+    private void dfs(int row, int col) {
+        
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) {
+            return;
+        }
+
+        if (visited[row][col] == VISITED) {
+            return;
+        }
+
+        visited[row][col] = VISITED;
+
+        if (grid[row][col] != ISLAND) {
+            return;
+        }
+
+        dfs(row + 1, col);
+        dfs(row - 1, col);
+        dfs(row, col + 1);
+        dfs(row, col - 1);
+    }
 
     public int numIslands(char[][] grid) {
-        row = grid.length;
-        column = grid[0].length;
-        int numOfIsland = 0;
+        this.grid = grid;
         
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
-                if (grid[i][j] == '1') {
-                    numOfIsland++;
-                    checkIsland(grid, i, j);
+        if (grid.length == 0) {
+            return 0;
+        }
+
+        if (grid[0].length == 0) {
+            return 0;
+        }
+
+        visited = new int[grid.length][grid[0].length];
+        
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                visited[i][j] = NOT_VISITED;
+            }
+        }
+
+        counter = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == ISLAND && visited[i][j] == NOT_VISITED) {
+                    counter++;
+                    dfs(i, j);
+                } else {
+                    visited[i][j] = VISITED;
                 }
             }
         }
 
-        return numOfIsland;
-    }
-
-    private void checkIsland(char[][] grid, int i, int j) {
-        
-        if (i < 0 || i >= row || j < 0 || j >= column || grid[i][j] == '0') {
-            return;
-        }
-
-        grid[i][j] = '0';
-        checkIsland(grid, i - 1, j);
-        checkIsland(grid, i + 1, j);
-        checkIsland(grid, i, j - 1);
-        checkIsland(grid, i, j + 1);
-
-        return;
+        return counter;
     }
 }
