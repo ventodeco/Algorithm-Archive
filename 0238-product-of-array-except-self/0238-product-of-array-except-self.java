d659bf5be39ed1b7bc09d91adcc21f23b0ceb1a2
Yosep Novento Nugroho <42646158+ventodeco@users.noreply.github.com>
2024-11-08 07:39:50 +0700

    Time: 3 ms (25.12%), Space: 57.3 MB (5.64%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,39 +1,31 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] productFromLeft = new int[nums.length];
-        int fromLeftTotal = 1;
+        int result = 1;
+        int resultWithoutZero = 1;
+        int zeroCounter = 0;
+
         for (int i = 0; i < nums.length; i++) {
-            fromLeftTotal *= nums[i];
-            productFromLeft[i] = fromLeftTotal;
+            if (nums[i] == 0) {
+                zeroCounter++;
+            }
+            resultWithoutZero *= nums[i];
+            if (nums[i] != 0) {
+                result *= nums[i];
+            }
         }
 
-        int[] productFromRight = new int[nums.length];
-        int fromRightTotal = 1;
-        for (int i = nums.length - 1; i >= 0; i--) {
-            fromRightTotal *= nums[i];
-            productFromRight[i] = fromRightTotal;
+        if (zeroCounter > 1) {
+            return new int[nums.length];
         }
 
-        int[] result = new int[nums.length];
-        int leftResult;
-        int rightResult;
         for (int i = 0; i < nums.length; i++) {
-            
-            if (i > 0) {
-                leftResult = productFromLeft[i - 1];
+            if (nums[i] == 0) {
+                nums[i] = result;
             } else {
-                leftResult = 1;
+                nums[i] = resultWithoutZero/nums[i];
             }
-            
-            if (i < nums.length - 1) {
-                rightResult = productFromRight[i + 1];
-            } else {
-                rightResult = 1;
-            }
-            
-            result[i] = leftResult * rightResult;
         }
 
-        return result;
+        return nums;
     }
 }
