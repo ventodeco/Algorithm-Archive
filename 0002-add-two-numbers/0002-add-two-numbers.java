ef914aff11a247c7931f9367a62f630061abb674
Yosep Novento Nugroho <42646158+ventodeco@users.noreply.github.com>
2024-11-30 17:52:44 +0700

    Time: 13 ms (5.16%), Space: 45.2 MB (6.73%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,56 +1,25 @@
+import java.math.BigInteger;
+
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        String number1 = "";
+        String number2 = "";
 
-        ListNode temp;
-        ListNode pointer;
-        ListNode result = null;
-        pointer = result;
-        int valueL1 = 0;
-        int valueL2 = 0;
-        int resultSum;
-        int carryOver = 0;
-        while (l1 != null || l2 != null) {
-            if (l1 != null) {
-                valueL1 = carryOver + l1.val;
-                carryOver = 0;
-                l1 = l1.next;
-            } 
-
-            if (l2 != null) {
-                valueL2 = carryOver + l2.val;
-                carryOver = 0;
-                l2 = l2.next;
-            }
-
-            resultSum = valueL1 + valueL2;
-            valueL1 = 0;
-            valueL2 = 0;
-            while (resultSum >= 10) {
-                resultSum -= 10;
-                carryOver += 1;
-            }
-
-            temp = new ListNode(resultSum);
-
-            if (result == null) {
-                result = temp;
-                pointer = result;
-            } else {
-                pointer.next = temp;
-                pointer = temp;
-            }
+        while (l1 != null) {
+            number1 = String.valueOf(l1.val) + number1;
+            l1 = l1.next;
         }
 
-        if (carryOver > 0) {
-            temp = new ListNode(carryOver);
+        while (l2 != null) {
+            number2 = String.valueOf(l2.val) + number2;
+            l2 = l2.next;
+        }
 
-            if (result == null) {
-                result = temp;
-                pointer = result;
-            } else {
-                pointer.next = temp;
-                pointer = temp;
-            }
+        String resultInString = new BigInteger(number1).add(new BigInteger(number2)).toString();
+
+        ListNode result = null;
+        for (int i = 0; i < resultInString.length(); i++) {
+            result = new ListNode(resultInString.charAt(i) - '0', result);
         }
 
         return result;
