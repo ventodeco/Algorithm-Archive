75df3f1b5cc4028c0db9ad0563a537a1cef31d17
Yosep Novento Nugroho <42646158+ventodeco@users.noreply.github.com>
2024-12-01 15:48:26 +0700

    Time: 9 ms (32.35%), Space: 47.7 MB (58.36%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,44 +1,23 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         
-        if (strs.length == 0) {
-            return List.of(List.of());
-        }
+        Map<String, List<String>> map = new HashMap<>();
 
-        Map<String, List<String>> groupAnagramMap = new HashMap<>();
-        String sortedString;
-        List<String> listString;
         for (int i = 0; i < strs.length; i++) {
-            sortedString = getSortedString(strs[i]);
-
-            listString = groupAnagramMap.get(sortedString);
-            if (listString != null) {
-                listString.add(strs[i]);
-            } else {
-                listString = new ArrayList<>();
-                listString.add(strs[i]);
-                groupAnagramMap.put(sortedString, listString);
-            }
+            char[] charArr = strs[i].toCharArray();
+            Arrays.sort(charArr);
+            String sortedString = Arrays.toString(charArr);
+            List<String> list = map.getOrDefault(sortedString, new ArrayList<>());
+            list.add(strs[i]);
+            map.put(sortedString, list);
         }
-        
+
         List<List<String>> result = new ArrayList<>();
-        for (Map.Entry<String, List<String>> groupAnagram : groupAnagramMap.entrySet()) {
-            result.add(groupAnagram.getValue());
-        }
-        
-        return result;
-    }
 
-    private String getSortedString(String str) {
-        char[] strFromChar = str.toCharArray();
-        
-        Arrays.sort(strFromChar);
-        
-        String newString = "";
-        for (int i = 0; i < strFromChar.length; i++) {
-            newString += strFromChar[i];
+        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+            result.add(entry.getValue());
         }
-        
-        return newString;
+
+        return result;
     }
 }
