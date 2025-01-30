7bfc9779bb6486f0cbe38522f71025a059992d47
Yosep Novento Nugroho <42646158+ventodeco@users.noreply.github.com>
2025-01-30 08:50:07 +0700

    Time: 1 ms (29.54%), Space: 42.7 MB (8.46%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,22 +1,19 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
 
         for (int i = 0; i < m; i++) {
             minHeap.add(nums1[i]);
         }
-        
+
         for (int i = 0; i < n; i++) {
             minHeap.add(nums2[i]);
         }
-        
-        int counter = 0;
-        while (! minHeap.isEmpty()) {
-            nums1[counter] = minHeap.poll();
-            counter++;
+
+        int it = 0;
+        while (!minHeap.isEmpty()) {
+            nums1[it] = minHeap.poll();
+            it++;
         }
-        
-        return;
     }
 }
