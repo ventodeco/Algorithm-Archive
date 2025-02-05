d910c7bec04db8c2974df05c6dc86ccf56db1a8d
Yosep Novento Nugroho <42646158+ventodeco@users.noreply.github.com>
2025-02-05 18:10:31 +0700

    Time: 14 ms (25.24%), Space: 49.9 MB (74.66%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,31 +1,22 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Map<Integer, Integer> counterNums = new HashMap<>();
-        
-        Integer counter;
-        for (Integer num : nums) {
-            
-            if (counterNums.containsKey(num)) {
-                counter = counterNums.get(num);
-                counter += 1;
-                counterNums.put(num, counter);
-                
-            } else {
-                counterNums.put(num, 1);
-            }
+
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> {
+            return b[1] - a[1];
+        });
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int val = map.getOrDefault(nums[i], 0);
+            val++;
+            map.put(nums[i], val);
         }
-        
-        Integer size = nums.length / 2;
-        Integer maxCounter = -1;
-        Integer numValue = -1;
-        for (Map.Entry<Integer, Integer> num: counterNums.entrySet()) {
-            
-            if (num.getValue() > maxCounter) {
-                maxCounter = num.getValue();
-                numValue = num.getKey();
-            }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            maxHeap.add(new int[]{entry.getKey(), entry.getValue()});
         }
-        
-        return maxCounter > size ? numValue : -1;
+
+        return maxHeap.poll()[0];
     }
 }
