417a300598bd3153e1bbc06e91da834d39bfc808
Yosep Novento Nugroho <42646158+ventodeco@users.noreply.github.com>
2025-02-10 00:46:32 +0700

    Time: 3 ms (74.69%), Space: 42.9 MB (83.32%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,17 +1,16 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        
         int left = 0;
         int right = s.length() - 1;
 
-        while (left < right) {
+        while (left <= right) {
 
-            if (! isAlphaNumeric(s.charAt(left))) {
+            if (! Character.isAlphabetic(s.charAt(left)) && ! Character.isDigit(s.charAt(left))) {
                 left++;
                 continue;
             }
 
-            if (! isAlphaNumeric(s.charAt(right))) {
+            if (! Character.isAlphabetic(s.charAt(right)) && ! Character.isDigit(s.charAt(right))) {
                 right--;
                 continue;
             }
@@ -25,9 +24,5 @@ class Solution {
         }
 
         return true;
-    }
-
-    private boolean isAlphaNumeric(char ch) {
-        return (ch >= '0' && ch <= '9') || (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z');
     }
 }
