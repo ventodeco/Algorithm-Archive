9a1de527641a1684210cca46b245694391ea7462
Yosep Novento Nugroho <42646158+ventodeco@users.noreply.github.com>
2025-02-12 10:16:06 +0700

    Time: 0 ms (100%), Space: 40.9 MB (10.49%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -5,17 +5,15 @@ class Solution {
             return 1;
         }
 
-        if (n == 2) {
-            return 2;
-        }
-        
-        int[] arr = new int[n];
-        arr[n - 1] = 1;
-        arr[n - 2] = 2;
-        for (int i = n - 3; i >= 0; i--) {
-            arr[i] = arr[i + 1] + arr[i + 2];
+        int[] dp = new int[n];
+
+        dp[0] = 1;
+        dp[1] = 2;
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
 
-        return arr[0];
+        return dp[n-1];
     }
 }
