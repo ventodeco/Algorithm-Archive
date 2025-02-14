3755088d2a6ecdee3e381e4930cb808fefc69d1b
Yosep Novento Nugroho <42646158+ventodeco@users.noreply.github.com>
2025-02-14 19:26:03 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,11 +1,25 @@
 class Solution {
+
+    private int[][] visited;
     private char[][] board;
-    
+    private Integer rowLength;
+    private Integer colLength;
+    private static int VISITED = 1;
+    private static int NOT_VISITED = 0;
+    private Integer keeper;
+    private String word;
+
     public boolean exist(char[][] board, String word) {
+        rowLength = board.length;
+        colLength = board[0].length;
+        this.word = word;
         this.board = board;
-        for (int row = 0; row < board.length; row++) {
-            for (int col = 0; col < board[row].length; col++) {
-                if (findWord(row, col, word, 0)) {
+
+        for (int row = 0; row < rowLength; row++) {
+            for (int col = 0; col < colLength; col++) {
+                keeper = 0;
+                visited = new int[rowLength][colLength];
+                if (deepDive(row, col)) {
                     return true;
                 }
             }
@@ -13,28 +27,49 @@ class Solution {
 
         return false;
     }
-    
-    private boolean findWord(int row, int col, String word, int iterator) {
-        
-        if (iterator >= word.length()) {
-            return true;
-        }
-        
-        if (row < 0 || col < 0 || row >= board.length || col >= board[0].length || word.charAt(iterator) != board[row][col]) {
+
+    private boolean deepDive(int row, int col) {
+        if (row < 0 || row >= rowLength || col < 0 || col >= colLength) {
             return false;
         }
 
-        board[row][col] = '#';
+        if (visited[row][col] == VISITED) {
+            return false;
+        }
 
-        if (findWord(row + 1, col, word, iterator + 1) ||
-            findWord(row - 1, col, word, iterator + 1) ||
-            findWord(row, col + 1, word, iterator + 1) ||
-            findWord(row, col - 1, word, iterator + 1)
-        ) {
+        visited[row][col] = VISITED;
+
+        if (board[row][col] != word.charAt(keeper)) {
+            visited[row][col] = NOT_VISITED;
+            return false;
+        }
+
+        keeper++;
+
+        if (keeper == word.length()) {
             return true;
         }
-        
-        board[row][col] = word.charAt(iterator);
+
+        if (deepDive(row + 1, col)) {
+            return true;
+        }
+
+        if (deepDive(row - 1, col)) {
+            return true;
+        }
+
+        if (deepDive(row, col + 1)) {
+            return true;
+        }
+
+        if (deepDive(row, col - 1)) {
+            return true;
+        }
+
+        if (keeper > 0) {
+            keeper--;
+        }
+
         return false;
     }
 }
