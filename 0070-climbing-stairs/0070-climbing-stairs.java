a19b1e9e686afa029d7b2f7ef1fd3ddc905c5899
Yosep Novento Nugroho <42646158+ventodeco@users.noreply.github.com>
2025-04-06 20:58:26 +0700

    Time: 0 ms (100%), Space: 40.4 MB (65.38%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,8 +1,7 @@
 class Solution {
     public int climbStairs(int n) {
-
-        if (n == 1) {
-            return 1;
+        if (n <= 2) {
+            return n;
         }
 
         int[] dp = new int[n];
@@ -14,6 +13,6 @@ class Solution {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
 
-        return dp[n-1];
+        return dp[n - 1];
     }
 }
