fbee2bea2a3033d3329f5f016ccda8cc2d4814d2
Yosep Novento Nugroho <42646158+ventodeco@users.noreply.github.com>
2025-04-10 11:05:59 +0700

    Time: 15 ms (77.65%), Space: 44.6 MB (57%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,18 +1,35 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
 
-        int[] dp = new int[amount + 1];
-        Arrays.fill(dp, amount + 1);
-        dp[0] = 0;
+
+        // amount 0 -> [0]
+        // amount 1 -> [-1]
+        // amount 2 -> [2]
+        // amount 3 -> [-1]
+
+        // amount val -> 11
+        // amount 0 -> [0]
+        // amount 1 -> [1]
+            // 1,2,5 -> 1 - 1 = 0 | 1 - 2 = -1 | 1 - 5 = -4 
+            // Min(amounts[1] -> amount + 1, 1 + amounts[0])| Min(amount + 1, 1) | 1
+            // amounts[1] = 1
+        // amount 2 -> [2]
+        // amount 3 -> [[1,2], [1,1,1], [2,1]]
+        // amount 4 -> [[1,1,1,1], [1,1,2], [1,2,1], [2,1,1], [2,2]]
+
+        int[] amounts = new int[amount + 1];
+        Arrays.fill(amounts, amount + 1);
+
+        amounts[0] = 0;
 
         for (int am = 1; am <= amount; am++) {
             for (int coin : coins) {
                 if (am - coin >= 0) {
-                    dp[am] = Math.min(1 + dp[am - coin], dp[am]);
+                    amounts[am] = Math.min(amounts[am], 1 + amounts[am - coin]);
                 }
             }
         }
 
-        return dp[amount] != amount + 1 ? dp[amount] : -1;
+        return amounts[amount] != amount + 1 ? amounts[amount] : -1;
     }
 }
