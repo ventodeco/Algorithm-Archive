78edde2005798e2a3b87eba991f6c7e2e9fa2e85
Yosep Novento Nugroho <42646158+ventodeco@users.noreply.github.com>
2025-04-11 21:15:49 +0700

    Time: 6 ms (30.43%), Space: 44.3 MB (36.26%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -3,20 +3,21 @@ class Solution {
         if (x < 0) {
             return false;
         }
-        
+
         String str = String.valueOf(x);
-        Integer left = 0;
-        Integer right = str.length() - 1;
-        
-        while (left < right) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left <= right) {
+
             if (str.charAt(left) != str.charAt(right)) {
                 return false;
             }
-            
+
             left++;
             right--;
         }
-        
+
         return true;
     }
 }
