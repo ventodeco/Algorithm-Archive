87aec0b8d4b8acbb5f0b32b14a7e4278f53cee38
Yosep Novento Nugroho <42646158+ventodeco@users.noreply.github.com>
2025-04-16 23:14:29 +0700

    Time: 5 ms (37.22%), Space: 44.4 MB (63.85%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -1,23 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
-        
-        ListNode temp;
+
         for (int i = 0; i < lists.length; i++) {
-            
-            temp = lists[i];
-            
-            while (temp != null) {
-                maxHeap.add(temp.val);
-                temp = temp.next;
+            ListNode node = lists[i];
+            while (node != null) {
+                maxHeap.add(node.val);
+                node = node.next;
             }
         }
-        
-        ListNode head = null;
-        while (! maxHeap.isEmpty()) {
-            head = new ListNode(maxHeap.poll(), head);
+
+        if (maxHeap.isEmpty()) {
+            return null;
         }
 
-        return head;
+        ListNode result = new ListNode(maxHeap.poll(), null);
+        while (!maxHeap.isEmpty()) {
+            result = new ListNode(maxHeap.poll(), result);
+        }
+
+        return result;
     }
 }
