c0b193ebe906dfc3b030e295ea7786b3a1a20110
Yosep Novento Nugroho <42646158+ventodeco@users.noreply.github.com>
2025-04-24 23:46:16 +0700

    Time: 2 ms (2.52%), Space: 43 MB (14.56%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -9,45 +9,30 @@
  * }
  */
 class Solution {
-
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        
-        if (list1 == null) {
-            return list2;
+
+        if (list1 == null && list2 == null) {
+            return null;
         }
-        
-        if (list2 == null) {
-            return list1;
-        }
-        
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+
         while (list1 != null) {
-            minHeap.add(list1.val);
+            maxHeap.add(list1.val);
             list1 = list1.next;
         }
-        
+
         while (list2 != null) {
-            minHeap.add(list2.val);
+            maxHeap.add(list2.val);
             list2 = list2.next;
         }
-        
-        ListNode head = new ListNode(-101);
-        ListNode tail = new ListNode(-101);
 
-        head.next = tail;
-        
-        ListNode keeper = head;
-        ListNode temp;
+        ListNode result = new ListNode(maxHeap.poll());
 
-        while (! minHeap.isEmpty()) {
-            temp = new ListNode(minHeap.poll(), tail);
-            keeper.next = temp;
-            keeper = keeper.next;
+        while (!maxHeap.isEmpty()) {
+            result = new ListNode(maxHeap.poll(), result);
         }
-        
-        keeper.next = null;
 
-        return head.next;
+        return result;
     }
 }
