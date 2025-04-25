70c721e8e63edcb5ffa8df9bca163652b978fe5e
Yosep Novento Nugroho <42646158+ventodeco@users.noreply.github.com>
2025-04-25 23:53:30 +0700

    Time: 14 ms (24.17%), Space: 49.4 MB (86.29%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,22 +1,19 @@
 class Solution {
     public int majorityElement(int[] nums) {
-
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> {
-            return b[1] - a[1];
-        });
-
         Map<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            int val = map.getOrDefault(nums[i], 0);
-            val++;
-            map.put(nums[i], val);
+            int temp = map.getOrDefault(nums[i], 0);
+            temp++;
+            map.put(nums[i], temp);
         }
+
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> b[1] - a[1]);
 
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
             maxHeap.add(new int[]{entry.getKey(), entry.getValue()});
         }
 
-        return maxHeap.poll()[0];
+        return maxHeap.peek()[0];
     }
 }
