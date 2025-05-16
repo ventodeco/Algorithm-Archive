9b8eca1b6ab60808ad1a44eff1be9dff932318ab
Yosep Novento Nugroho <42646158+ventodeco@users.noreply.github.com>
2025-05-16 23:42:42 +0700

    Time: 1 ms (38.73%), Space: 43.4 MB (38.51%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -2,6 +2,7 @@ class Solution {
     public int minCostClimbingStairs(int[] cost) {
 
         int n = cost.length;
+
         int[] dp = new int[n];
         dp[0] = cost[0];
         dp[1] = cost[1];
