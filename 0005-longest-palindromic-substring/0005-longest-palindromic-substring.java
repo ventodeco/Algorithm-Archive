80fb667615e670da0cd4d0dd46bdc85169faac9b
Yosep Novento Nugroho <42646158+ventodeco@users.noreply.github.com>
2025-05-30 21:34:06 +0700

    Time: 224 ms (22.64%), Space: 46.2 MB (16.75%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,46 +1,49 @@
 class Solution {
     public String longestPalindrome(String s) {
-        
-        String result = String.valueOf(s.charAt(0));
+
+        String palindrome = String.valueOf(s.charAt(0));
         for (int i = 1; i < s.length(); i++) {
-            // odd
+            
             int left = i - 1;
             int right = i + 1;
-            String strTemp = String.valueOf(s.charAt(i));
+            String tempVal = String.valueOf(s.charAt(i));
             while (left >= 0 && right < s.length()) {
+
                 if (s.charAt(left) != s.charAt(right)) {
                     break;
                 }
-                strTemp = String.valueOf(s.charAt(left)) + strTemp + String.valueOf(s.charAt(right));
+
+                tempVal = s.charAt(left) + tempVal + s.charAt(right);
                 left--;
                 right++;
             }
 
-            if (result.length() < strTemp.length()) {
-                result = strTemp;
+            if (tempVal.length() > palindrome.length()) {
+                palindrome = tempVal;
             }
 
-            // even
             if (s.charAt(i) == s.charAt(i - 1)) {
-                strTemp = String.valueOf(s.charAt(i - 1)) + String.valueOf(s.charAt(i));
                 left = i - 2;
                 right = i + 1;
-
+                tempVal = String.valueOf(s.charAt(i - 1)) + String.valueOf(s.charAt(i));
+                
                 while (left >= 0 && right < s.length()) {
+
                     if (s.charAt(left) != s.charAt(right)) {
                         break;
                     }
-                    strTemp = String.valueOf(s.charAt(left)) + strTemp + String.valueOf(s.charAt(right));
+
+                    tempVal = s.charAt(left) + tempVal + s.charAt(right);
                     left--;
                     right++;
                 }
+            }
 
-                if (result.length() < strTemp.length()) {
-                    result = strTemp;
-                }
+            if (tempVal.length() > palindrome.length()) {
+                palindrome = tempVal;
             }
         }
 
-        return result;
+        return palindrome;
     }
 }
