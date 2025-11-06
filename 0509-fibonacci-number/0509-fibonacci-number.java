b4f26e45da164f95a9c150216283e6c0ff1aa59b
Yosep Novento Nugroho <42646158+ventodeco@users.noreply.github.com>
2025-11-06 07:46:23 +0700

    Time: 0 ms (100%), Space: 41.8 MB (6.33%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,9 +1,9 @@
 class Solution {
     public int fib(int n) {
 
-        if (n == 0) return 0;
-
-        if (n == 1) return 1;
+        if (n < 2) {
+            return n;
+        }
         
         int[] dp = new int[n + 1];
 
